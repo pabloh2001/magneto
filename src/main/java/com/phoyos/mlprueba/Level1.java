a0867005fe35b478d54cc20e5f3d648e64ec1493
pabloh2001 <phoyos06@gmail.com>
2022-08-10 17:36:08 -0500

    level one finished

---
+++ src/main/java/com/phoyos/mlprueba/Level1.java
@@ -0,0 +1,2 @@
+package com.phoyos.mlprueba;public class Level1 {
+}
---
+++ src/main/java/com/phoyos/mlprueba/MlPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.phoyos.mlprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlPruebaApplication.class, args);
+	}
+
+}
